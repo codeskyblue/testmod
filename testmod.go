0f95044656b5498945d8e590d66c16429005fe97
codeskyblue <codeskyblue@gmail.com>
2018-11-19 19:18:09 +0800

    first commit

---
+++ testmod.go
@@ -0,0 +1,10 @@
+package testmod
+
+// https://roberto.selbach.ca/intro-to-go-modules/
+
+import "fmt"
+
+// Hi returns a friendly greeting
+func Hi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
