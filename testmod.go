3e5cf9c80e99bd3ba387cfc4f5981ea64074277d
codeskyblue <codeskyblue@gmail.com>
2018-11-19 19:39:15 +0800

    Change Hi to allow multlang

--- testmod.go
+++ testmod.go
@@ -1,10 +1,19 @@
+// https://roberto.selbach.ca/intro-to-go-modules/
 package testmod
 
-// https://roberto.selbach.ca/intro-to-go-modules/
-
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hi returns a friendly greeting
-func Hi(name string) string {
-	return fmt.Sprintf("Hi, %s!", name)
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "cn":
+		return fmt.Sprintf("你好, %s", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
